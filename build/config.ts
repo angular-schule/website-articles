3c9984feb2a54e273457f0c1f2adde4c9163f152
Ferdinand Malcher <ferdinand@malcher.media>
2024-11-14 13:42:35 +0100

    build: blog data

---
+++ build/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  markdownBaseUrl: 'https://angular-schule.github.io/website-articles/',
+};
---
+++ build/types.ts
@@ -0,0 +1,49 @@
+export interface BlogEntryMeta {
+  title: string;
+  hidden: boolean;
+  author: string;
+  mail: string;
+  bio?: string;
+  twitter?: string;
+
+  author2?: string;
+  mail2?: string;
+  bio2?: string;
+  twitter2?: string;
+
+  published: Date | string; // Date on backend, ISO-formated string on client, foo!
+  'last-change'?: Date | string; // Date on backend, ISO-formated string on client, foo!
+  keywords?: string[];
+  language: string;
+  thumbnail: string;
+  'darken-thumbnail': boolean;
+  category: string; // schule, buch, general
+  sticky?: boolean;
+}
+
+
+export interface BlogEntry {
+  slug: string; // SEO-friendly path
+  html: string;
+  error: string;
+  meta: BlogEntryMeta;
+}
+
+export interface BlogEntryLight {
+  slug: string;
+  html: string;
+  meta: {
+    title: string;
+
+    author: string;
+    mail: string;
+    twitter?: string;
+
+    author2?: string;
+    mail2?: string;
+    twitter2?: string;
+
+    published: Date | string;
+    thumbnail: string
+  };
+}
