71d29a85e5170b7ebeb5a5249486f6e50f6bc2c9
The Buildbot <buildbot@angular2buch.de>
2026-02-05 12:11:33 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/website-articles/commit/de68c9daf4ad7c4815e720af4ad4cffd1b6ec6de

--- .github/workflows/purge-cache.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${PURGE_ZONE}/purge_cache" \
-     -H "Authorization: Bearer ${PURGE_API_TOKEN}" \
-     -H "Content-Type: application/json" \
-     --data '{"purge_everything":true}'
